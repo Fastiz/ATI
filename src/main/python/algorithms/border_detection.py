Laplacian (threshold), ImageWrapper normalization, fixes

src/main/python/algorithms/border_detection.py
@@ -41,6 +41,8 @@ def prewitt_border_detection(channel: np.ndarray):
             dx = apply_mask(channel_cpy, x, y, prewitt_x_mask)
             dy = apply_mask(channel_cpy, x, y, prewitt_y_mask)
             channel[x, y] = int(math.sqrt((dx ** 2) + (dy ** 2)))
+            if channel[x, y] > 255:
+                channel[x, y] = 255
 
     return channel
 
@@ -67,22 +69,18 @@ def sobel_border_detection(channel: np.ndarray):
             dx = apply_mask(channel_cpy, x, y, sobel_x_mask)
             dy = apply_mask(channel_cpy, x, y, sobel_y_mask)
             channel[x, y] = int(math.sqrt((dx ** 2) + (dy ** 2)))
+            if channel[x, y] > 255:
+                channel[x, y] = 255
 
     return channel
 
 
-def laplace_border_detection(channel: np.ndarray, mask: np.ndarray):
+def laplace_border_detection(channel: np.ndarray, mask: np.ndarray, threshold: float = None):
     h, w = channel.shape
 
     channel_cpy = channel.copy()
 
     channel_matrix = channel.copy()
-
-    laplace_mask = np.array([
-        [0, -1, 0],
-        [-1, 4, -1],
-        [0, -1, 0]
-    ])
 
     for x in range(w):
         for y in range(h):
@@ -94,20 +92,28 @@ def laplace_border_detection(channel: np.ndarray, mask: np.ndarray):
 
     for x in range(w):
         for y in range(h - 1):
-            if channel_matrix[x, y] == 0:
-                if y - 1 >= 0 and np.sign(channel_matrix[x, y - 1]) != np.sign(channel_matrix[x, y + 1]):
-                    channel[x, y] = 255
+            if threshold is None:
+                if channel_matrix[x, y] == 0:
+                    if y - 1 >= 0 and np.sign(channel_matrix[x, y - 1]) != np.sign(channel_matrix[x, y + 1]):
+                        channel[x, y] = 255
+                else:
+                    if np.sign(channel_matrix[x, y]) != np.sign(channel_matrix[x, y + 1]):
+                        channel[x, y] = 255
             else:
-                if np.sign(channel_matrix[x, y]) != np.sign(channel_matrix[x, y + 1]):
+                if abs(channel_matrix[x, y]) + abs(channel_matrix[x, y + 1]) >= threshold:
                     channel[x, y] = 255
 
     for y in range(h):
         for x in range(w - 1):
-            if channel_matrix[x, y] == 0:
-                if x - 1 >= 0 and np.sign(channel_matrix[x - 1, y]) != np.sign(channel_matrix[x + 1, y]):
-                    channel[x, y] = 255
+            if threshold is None:
+                if channel_matrix[x, y] == 0:
+                    if x - 1 >= 0 and np.sign(channel_matrix[x - 1, y]) != np.sign(channel_matrix[x + 1, y]):
+                        channel[x, y] = 255
+                else:
+                    if np.sign(channel_matrix[x, y]) != np.sign(channel_matrix[x + 1, y]):
+                        channel[x, y] = 255
             else:
-                if np.sign(channel_matrix[x, y]) != np.sign(channel_matrix[x + 1, y]):
+                if abs(channel_matrix[x, y]) + abs(channel_matrix[x + 1, y]) >= threshold:
                     channel[x, y] = 255
 
     return channel
@@ -120,7 +126,6 @@ def generate_log_mask(mask_size: int, sigma: float):
     for x in range(-mask_border_offset, mask_border_offset + 1):
         for y in range(-mask_border_offset, mask_border_offset + 1):
             mask[x + mask_border_offset][y + mask_border_offset] = -(1 / (math.sqrt(2 * math.pi) * sigma ** 3)) * (
-                        2 - (x ** 2 + y ** 2) / sigma ** 2) * math.exp(-(x ** 2 + y ** 2) / (2 * sigma ** 2))
+                    2 - (x ** 2 + y ** 2) / sigma ** 2) * math.exp(-(x ** 2 + y ** 2) / (2 * sigma ** 2))
 
     return mask
-

src/main/python/utils/ImageWrapper.py
@@ -106,9 +106,20 @@ class ImageWrapper:
     def draw_image(self):
         channels = []
         for channel in self.channels:
-            if np.any(channel < 0) or np.any(channel > 255):
-                raise ValueError("At least one pixel value outside range.")
-            channel_int = np.uint8(channel)
+            channel_int = self.normalize_channel(np.uint8(channel), 0, 255)
             channels.append(Image.fromarray(channel_int, 'L'))
         self.image_element = Image.merge(self.mode, channels)
         return self.image_element
+
+    def normalize(self):
+        for channel in self.channels:
+            self.normalize_channel(channel, 0, 255)
+
+    @staticmethod
+    def normalize_channel(channel: np.ndarray, new_min: float, new_max: float):
+        oldMax = channel.max()
+        oldMin = channel.min()
+        for i in range(channel.shape[1]):
+            for j in range(channel.shape[0]):
+                channel[i, j] = (((channel[i, j] - oldMin) * (new_max - new_min)) / (oldMax - oldMin)) + new_min
+        return channel