eeef8751834f34d7d15a5b6338d70736d866d6ea
fastiz <fastiz@itba.edu.ar>
2020-09-23 21:24:33 -0300

    now bilateral filter uses intensity as a color vector

--- src/main/python/algorithms/bilateral_filter.py
+++ src/main/python/algorithms/bilateral_filter.py
@@ -1,31 +1,35 @@
-from typing import Tuple
+from typing import Tuple, List
 import numpy as np
 from src.main.python.utils.ImageWrapper import ImageWrapper
 
 
-def calculate_w(i: int, j: int, w: int, k: int, sigma_s: float, sigma_r: float, channel: np.array):
+def calculate_w(i: int, j: int, w: int, k: int, sigma_s: float, sigma_r: float, channels: List[np.array]) -> float:
+    ij_value = np.array([channel[i, j]/255 for channel in channels])
+    wk_value = np.array([channel[w, k]/255 for channel in channels])
+
     val_1: float = -(np.power(i - k, 2) + np.power(j - w, 2)) / (2 * np.power(sigma_s, 2))
-    val_2: float = -(np.power(channel[i, j]/255 - channel[w, k]/255, 2)) / (2 * np.power(sigma_r, 2))
+    val_2: float = -(np.power(np.linalg.norm(ij_value - wk_value), 2)) / (2 * np.power(sigma_r, 2))
     return np.exp(val_1 + val_2)
 
 
-def calculate_filter(channel: np.array, i: int, j: int, window_dim: Tuple[int, int],
-                     sigma_s: float, sigma_r: float) -> float:
+def calculate_filter(channels: List[np.array], i: int, j: int, window_dim: Tuple[int, int],
+                     sigma_s: float, sigma_r: float) -> List[float]:
     window_w, window_h = window_dim
-    image_w, image_h = channel.shape
+    image_w, image_h = channels[0].shape
 
     max_h = int((window_h - 1)/2)
     max_w = int((window_w - 1)/2)
 
-    nominator: float = 0
-    denominator: float = 0
+    nominators: List[float] = [0] * len(channels)
+    denominators: List[float] = [0] * len(channels)
     for k in range(max(0, i - max_h), min(image_h, i + max_h + 1)):
         for w in range(max(0, j - max_w), min(image_w, j + max_w + 1)):
-            aux = calculate_w(i, j, w, k, sigma_s, sigma_r, channel)
-            nominator += channel[k, w] * aux
-            denominator += aux
+            aux = calculate_w(i, j, w, k, sigma_s, sigma_r, channels)
+            for i, channel in zip(range(len(channels)), channels):
+                nominators[i] += channel[k, w] * aux
+                denominators[i] += aux
 
-    return nominator/denominator
+    return [nominator/denominator for nominator, denominator in zip(nominators, denominators)]
 
 
 def bilateral_filter(image: ImageWrapper, window_dim: Tuple[int, int], sigma_s: float, sigma_r: float) -> ImageWrapper:
@@ -37,10 +41,11 @@ def bilateral_filter(image: ImageWrapper, window_dim: Tuple[int, int], sigma_s: 
     new_image: ImageWrapper = ImageWrapper.from_dimensions(w, h, image.get_mode())
 
     new_channels = [np.zeros(shape=(h, w))] * len(image.channels)
-    for (channel, new_channel) in zip(image.channels, new_channels):
-        for i in range(h):
-            for j in range(w):
-                new_channel[i, j] = calculate_filter(channel, i, j, window_dim, sigma_s, sigma_r)
+    for i in range(h):
+        for j in range(w):
+            values = calculate_filter(image.channels, i, j, window_dim, sigma_s, sigma_r)
+            for val, channel in zip(values, new_channels):
+                channel[i, j] = val
 
     new_image.channels = new_channels
     new_image.draw_image()
